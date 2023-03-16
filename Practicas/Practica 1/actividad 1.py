272ccfe693aab0aee82f3c5affff9ee68184f88c
Khazius5 <aguuspeirano.56@gmail.com>
2023-03-16 14:39:36 -0300

    Primer Ejercicio

---
+++ Practicas/Practica 1/actividad 1.py
@@ -0,0 +1,41 @@
+from random import choice, randrange
+from datetime import datetime 
+
+#Operadores posibles
+operators = ["+","-","*",]
+#Cantidad de cuentas
+times = 5
+#Contador inicial de tiempo
+#Esto toma la fecha y hora actual
+init_time = datetime.now()
+
+print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
+correct_answers = 0
+for i in range(0, times):
+    #Se eligen numeros y operadores al azar
+    number_1 = randrange(10)
+    number_2 = randrange(10)
+    operators = choice(operators)
+    #Se imprime la cuenta
+    print(f"{i+1}-- ¿Cuanto es {number_1} {operators} {number_2}?")
+    #Le pedimos al usuario el resultado 
+    result = input("Resultado: ")
+    #Comparamos la respuesta del usuario con el resultado de la operación
+    if int(result) == eval(f"{number_1} {operators} {number_2}"):
+        print("¡Respuesta correcta!")
+        correct_answers += 1
+    else:
+        print("Respuesta incorrecta")
+
+#Al terminar toda la cantidad de cuentas por resolver. 
+#Se vuelve a tomar la fecha y hora. 
+
+end_time = datetime.now()
+
+#Restando las fechas obtenemos el tiempo transcurrido
+total_time = end_time - init_time
+
+#Mostramos ese tiempo en segundos  
+print(f"\n Tardaste {total_time.seconds} segundos.")
+#Mostramos las respuestas correctas e incorrectas
+print(f"Obtuviste {correct_answers} respuestas correctas y {times - correct_answers} respuestas incorrectas.") 
