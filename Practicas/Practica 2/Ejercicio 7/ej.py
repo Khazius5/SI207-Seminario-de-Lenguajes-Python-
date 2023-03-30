5e836401c2ab538d2f933cf779073e67d9a1c5e6
Khazius5 <aguuspeirano.56@gmail.com>
2023-03-30 12:15:23 -0300

    :white_check_mark:

--- Practicas/Practica 2/Ejercicio 7/ej.py
+++ Practicas/Practica 2/Ejercicio 7/ej.py
@@ -2,6 +2,21 @@
 #palabras sin distinguir entre mayúsculas y minúsculas, en la frase.
 
 from text import TEXT as txt
+import string
+
 
 words = txt.split()
-f
+print(words)
+count = 0
+is_letter = False
+for word in words:
+    print(word)
+    for w in word:
+        if (w in string.ascii_letters):
+            is_letter = True
+        else:
+            is_letter = False
+    if (is_letter == True):
+        count += 1
+
+print(f"La cantidad de palabras en el texto es de: {count}")
